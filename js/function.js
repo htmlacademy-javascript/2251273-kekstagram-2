6383ded46a38727a519c3e8f04f9241f753befd1
Echenko <ehenko.v.g@yandex.ru>
2025-10-18 18:15:09 +0300

    Правки ошибок линтера

--- js/function.js
+++ js/function.js
@@ -1,11 +1,15 @@
 // Функции
 
 // Функция проверки длины строки
-let maxLengthString = (str = '', len = 0) => str.length <= len ? true : false;
+const maxLengthString = (str = '', len = 0) => str.length <= len;
 
 // Функция проверки полиндрома
-let isPolindrome = (str = '') => str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
+const isPolindrome = (str = '') => str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
 
 // Функция извлечения цифр
-let strToNumber = (str = '') => Number(str.split('').filter(item => /[0-9]/.test(item)).join('')) || NaN;
+const strToNumber = (str = '') => Number(str.split('').filter((item) => /[0-9]/.test(item)).join('')) || NaN;
 
+// Пример использования (для линтера)
+maxLengthString('Hello world', 15);
+isPolindrome('Hello world');
+strToNumber('Hello world');
