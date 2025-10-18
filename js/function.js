37300ea3678821f995489d15b7fdce12bf21eb06
Echenko <ehenko.v.g@yandex.ru>
2025-10-18 18:00:11 +0300

    module2-task1

---
+++ js/function.js
@@ -0,0 +1,11 @@
+// Функции
+
+// Функция проверки длины строки
+let maxLengthString = (str = '', len = 0) => str.length <= len ? true : false;
+
+// Функция проверки полиндрома
+let isPolindrome = (str = '') => str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
+
+// Функция преобразования строки в число
+let strToNumber = (str = '') => Number(str.split('').filter(item => /[0-9]/.test(item)).join('')) || NaN;
+
