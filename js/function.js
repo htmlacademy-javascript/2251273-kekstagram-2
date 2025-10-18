b15cc0901c48ad0cc18c86f66b664987095fbe87
Echenko <ehenko.v.g@yandex.ru>
2025-10-18 18:05:13 +0300

    module2-task1 правка названия функций

--- js/function.js
+++ js/function.js
@@ -6,6 +6,6 @@ let maxLengthString = (str = '', len = 0) => str.length <= len ? true : false;
 // Функция проверки полиндрома
 let isPolindrome = (str = '') => str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
 
-// Функция преобразования строки в число
+// Функция извлечения цифр
 let strToNumber = (str = '') => Number(str.split('').filter(item => /[0-9]/.test(item)).join('')) || NaN;
 
