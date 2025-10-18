086206c13119e80d90cda136800c2a524be2f4d8
Echenko <ehenko.v.g@yandex.ru>
2025-10-18 19:32:13 +0300

    Исправил имена и модифицировал функцию

--- js/function.js
+++ js/function.js
@@ -1,15 +1,15 @@
 // Функции
 
 // Функция проверки длины строки
-const maxLengthString = (str = '', len = 0) => str.length <= len;
+const checkLengthString = (str = '', len = 0) => str.length <= len;
 
 // Функция проверки полиндрома
-const isPolindrome = (str = '') => str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
+const isPolindrome = (str = '') => !str ? false : str.toLowerCase().replaceAll (' ', '') === str.toLowerCase().split('').reverse().join('').replaceAll (' ', '');
 
 // Функция извлечения цифр
 const strToNumber = (str = '') => Number(str.split('').filter((item) => /[0-9]/.test(item)).join('')) || NaN;
 
 // Пример использования (для линтера)
-maxLengthString('Hello world', 15);
-isPolindrome('Hello world');
+checkLengthString('Hello world', 15);
+isPolindrome('');
 strToNumber('Hello world');
