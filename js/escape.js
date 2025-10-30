62de3a0f1f38ab7c694c946de89d87edfe87b191
Echenko <ehenko.v.g@yandex.ru>
2025-10-30 18:06:20 +0300

    module9-task1

--- js/escape.js
+++ js/escape.js
@@ -1,22 +1,21 @@
 // Модуль обработки клавиш ESC
 // Импорт
 import { closeBigPicture } from './big-picture.js';
-import { closeForm, formStatus } from './form.js';
+import { closeForm } from './form.js';
 
 // Константы
-const bigPicture = document.querySelector('.big-picture');
-const bigPictureStaus = bigPicture.classList.contains('hidden');
-const imgUpload = document.querySelector('.img-upload__overlay');
-
 const form = document.querySelector('.img-upload__form');
 const inputDescription = form.querySelector('.text__description');
 const textHashtags = form.querySelector('.text__hashtags');
-
+const bigPicture = document.querySelector('.big-picture');
+const imgUpload = document.querySelector('.img-upload__overlay');
 
 // Функция проверки нажатия клавиши ESC
 const checkEscape = (evt) => {
+  const bigPictureStaus = bigPicture.classList.contains('hidden');
+  const formStatus = imgUpload.classList.contains('hidden');
   if (evt.key === 'Escape') {
-    if (bigPictureStaus) {
+    if (!bigPictureStaus) {
       closeBigPicture();
       document.removeEventListener('keydown', checkEscape);
     } else if (!formStatus && evt.target !== inputDescription && evt.target !== textHashtags) {
@@ -36,6 +35,5 @@ const offEscapeClick = () => {
   document.removeEventListener('keydown', checkEscape);
 };
 
-//
 // Экспорт
 export { onEscapeClick, offEscapeClick };
