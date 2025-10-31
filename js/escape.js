395d50d5482f06c0fc54e48c42256e517df11849
Echenko <ehenko.v.g@yandex.ru>
2025-10-31 12:37:19 +0300

    last

--- js/escape.js
+++ js/escape.js
@@ -1,18 +1,29 @@
 // Модуль обработки клавиш ESC
 // Импорт
 import { closeBigPicture } from './big-picture.js';
+import { closeForm, formStatus } from './form.js';
 
 // Константы
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureStaus = bigPicture.classList.contains('hidden');
+const imgUpload = document.querySelector('.img-upload__overlay');
+
+const form = document.querySelector('.img-upload__form');
+const inputDescription = form.querySelector('.text__description');
+const textHashtags = form.querySelector('.text__hashtags');
+
 
 // Функция проверки нажатия клавиши ESC
 const checkEscape = (evt) => {
   if (evt.key === 'Escape') {
     if (bigPictureStaus) {
       closeBigPicture();
+
+      document.removeEventListener('keydown', checkEscape);
+    } else if (!formStatus && evt.target !== inputDescription && evt.target !== textHashtags) {
+      closeForm();
+      document.removeEventListener('keydown', checkEscape);
     }
-    document.removeEventListener('keydown', checkEscape);
   }
 };
 
