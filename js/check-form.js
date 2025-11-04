f5cd3f6f3269beb238fb1d5a22edf4f05abf28b5
Echenko <ehenko.v.g@yandex.ru>
2025-11-04 19:29:55 +0300

    fix

--- js/check-form.js
+++ js/check-form.js
@@ -36,7 +36,7 @@ const checkButtonSubmit = () => {
 
 // Валидатор хэштега
 const hashtagValidator = (hashtag) => {
-  const regexp = /^#[a-zа-яё0-9_]*$/gi;
+  const regexp = /^#[a-z\u0430-\u044F\u04510-9_]*$/gi;
   return regexp.test(hashtag);
 };
 
