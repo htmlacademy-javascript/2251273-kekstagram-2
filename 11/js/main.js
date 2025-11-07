642b6c5529cff9928da980778361b55d8705f913
Keks <keks@htmlacademy.ru>
2025-11-07 20:51:57 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,6 +1,5 @@
 // Модуль главного скрипта
 // Импорт
-// import { getDataCards, setDataCards } from './data.js';
 import { renderCards } from './cards.js';
 import { setFormAttributes } from './form.js';
 import { getDataApi } from './api.js';
@@ -9,19 +8,6 @@ import { showMessageLoadError } from './message.js';
 
 // Отрисовка карточек
 getDataApi(renderCards, showMessageLoadError);
-// console.log(getDataApi());
-// const cards = getDataApi();
-// renderCards(cards);
-
-// (async () => {
-//   try {
-//     const photos = await getDataApi();
-//     renderCards(photos);
-//     // setPhotoData(photos);
-//   } catch {
-//     showMessageLoadError();
-//   }
-// }) ();
 
 // Установка атрибутов формы
 setFormAttributes();
