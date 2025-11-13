d96cd359aad2868170077c709f499b93f4a38023
Echenko <ehenko.v.g@yandex.ru>
2025-11-13 08:49:55 +0300

    +

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { setDataCards } from './data.js';
 import { renderCards } from './cards.js';
 import { onFormChange } from './form.js';
 
+
 (async () => {
   try {
     const cards = await getDataApi();
